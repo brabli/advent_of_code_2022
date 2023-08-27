4fb1281de313ca2ee2b24157e27e61686f18274f
brabli <67018167+Brabli@users.noreply.github.com>
2023-08-27 23:29:45 +0100

    Begin to write solving code

--- src/day11/part1.rs
+++ src/day11/part1.rs
@@ -1,5 +1,22 @@
+use self::monkey::{init_monkeys, Monkey, Name};
+
 mod monkey;
 
 pub fn solve() -> i32 {
+    let m = init_monkeys();
+    let new_worry = m.first().unwrap().calc_new_worry.as_ref()(12);
+    dbg!(new_worry);
     0
+}
+
+fn monkey_turn(monkey: Monkey) {}
+
+fn find_monkey_by_name(name: Name, monkies: &[Monkey; 8]) -> &Monkey {
+    for monkey in monkies {
+        if monkey.name == name {
+            return monkey;
+        }
+    }
+
+    panic!("Could not find a monkey with name {:?}", name);
 }
