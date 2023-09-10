1984e0d83d7d6619a0248d3a7a37a2151214def2
brabli <67018167+Brabli@users.noreply.github.com>
2023-09-10 15:22:31 +0100

    Finish part 1

--- src/day11/part1.rs
+++ src/day11/part1.rs
@@ -1,22 +1,64 @@
-use self::monkey::{init_monkeys, Monkey, Name};
+use monkey::*;
 
 mod monkey;
 
+// #[allow(unused)]
 pub fn solve() -> i32 {
-    let m = init_monkeys();
-    let new_worry = m.first().unwrap().calc_new_worry.as_ref()(12);
-    dbg!(new_worry);
-    0
-}
+    let mut monkies = init_monkeys();
+    let mut mods: Vec<MonkeyMod> = vec![];
 
-fn monkey_turn(monkey: Monkey) {}
+    for _i in 0..20 {
+        for monkey in &mut monkies {
+            if monkey.name == Name::Zero {
+                println!("{}", monkey.inspections);
+            }
 
-fn find_monkey_by_name(name: Name, monkies: &[Monkey; 8]) -> &Monkey {
-    for monkey in monkies {
-        if monkey.name == name {
-            return monkey;
+            for m in &mut mods {
+                if m.name == monkey.name {
+                    monkey.held_items.push(m.item);
+                }
+            }
+
+            mods.retain(|m| m.name != monkey.name);
+
+            println!("Number of items held: {}", monkey.held_items.len());
+            for item in &monkey.held_items {
+                let increased_worry = monkey.increase_worry_level_closure.as_ref()(*item);
+                monkey.inspections += 1;
+                let reduced_worry = reduce_worry_level(increased_worry);
+                let worry_check = check_worry_is_divisible(monkey.worry_divisor, reduced_worry);
+                let throwing_to_monkey_name = monkey.throwing_to_monkey(worry_check);
+
+                mods.push(MonkeyMod {
+                    name: throwing_to_monkey_name.clone(),
+                    item: reduced_worry,
+                });
+            }
+
+            monkey.held_items.clear();
         }
     }
 
-    panic!("Could not find a monkey with name {:?}", name);
+    let mut nums = monkies
+        .into_iter()
+        .map(|m| m.inspections)
+        .collect::<Vec<usize>>();
+
+    nums.sort_by(|a, b| b.cmp(a));
+
+    let first_largest = nums.first().unwrap();
+    let second_largest = nums.get(1).unwrap();
+
+    println!(
+        "First largest: {}\nSecond largest: {}",
+        first_largest, second_largest
+    );
+
+    (first_largest * second_largest).try_into().unwrap()
+}
+
+#[derive(Debug)]
+struct MonkeyMod {
+    name: Name,
+    item: usize,
 }
--- src/day11/part1/monkey.rs
+++ src/day11/part1/monkey.rs
@@ -1,15 +1,54 @@
+use std::rc::Rc;
+
+#[derive(Clone)]
 pub struct Monkey {
     pub name: Name,
-    held_items: Vec<usize>,
-    pub calc_new_worry: Box<dyn Fn(usize) -> usize>,
-    worry_divisor: usize,
-    success_monkey: Name,
-    failure_monkey: Name,
+    pub held_items: Vec<usize>,
+    pub increase_worry_level_closure: Rc<dyn Fn(usize) -> usize>,
+    pub worry_divisor: usize,
+    pub success_monkey: Name,
+    pub failure_monkey: Name,
+    pub inspections: usize,
+}
+
+impl std::fmt::Debug for Monkey {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Monkey")
+            .field("name", &self.name)
+            .field("held_items", &self.held_items)
+            .field("worry_divisor", &self.worry_divisor)
+            .field("success_monkey", &self.success_monkey)
+            .field("failure_monkey", &self.failure_monkey)
+            .field("inspections", &self.inspections)
+            .finish()
+    }
+}
+
+pub fn reduce_worry_level(worry_level: usize) -> usize {
+    worry_level / 3
+}
+
+pub fn check_worry_is_divisible(divisor: usize, worry: usize) -> bool {
+    worry % divisor == 0
+}
+
+pub fn find_monkey<'a>(name: &Name, monkies: &'a [Monkey; 8]) -> &'a Monkey {
+    for m in monkies {
+        if m.name == *name {
+            return m;
+        }
+    }
+
+    panic!();
 }
 
 impl Monkey {
-    pub fn check_worry(&self, worry: usize) -> bool {
-        worry % self.worry_divisor == 0
+    pub fn throwing_to_monkey(&self, check_result: bool) -> &Name {
+        if check_result {
+            &self.success_monkey
+        } else {
+            &self.failure_monkey
+        }
     }
 }
 
@@ -17,73 +56,81 @@ pub fn init_monkeys() -> [Monkey; 8] {
     let monkey_0 = Monkey {
         name: Name::Zero,
         held_items: vec![78, 53, 89, 51, 52, 59, 58, 85],
-        calc_new_worry: Box::new(|old| old * 3),
+        increase_worry_level_closure: Rc::new(|old| old * 3),
         worry_divisor: 5,
         success_monkey: Name::Two,
         failure_monkey: Name::Seven,
+        inspections: 0,
     };
 
     let monkey_1 = Monkey {
         name: Name::One,
         held_items: vec![64],
-        calc_new_worry: Box::new(|old| old + 7),
+        increase_worry_level_closure: Rc::new(|old| old + 7),
         worry_divisor: 2,
         success_monkey: Name::Three,
         failure_monkey: Name::Six,
+        inspections: 0,
     };
 
     let monkey_2 = Monkey {
         name: Name::Two,
         held_items: vec![71, 93, 65, 82],
-        calc_new_worry: Box::new(|old| old + 5),
+        increase_worry_level_closure: Rc::new(|old| old + 5),
         worry_divisor: 13,
         success_monkey: Name::Five,
         failure_monkey: Name::Four,
+        inspections: 0,
     };
 
     let monkey_3 = Monkey {
         name: Name::Three,
         held_items: vec![67, 73, 95, 75, 56, 74],
-        calc_new_worry: Box::new(|old| old + 8),
+        increase_worry_level_closure: Rc::new(|old| old + 8),
         worry_divisor: 19,
         success_monkey: Name::Six,
         failure_monkey: Name::Zero,
+        inspections: 0,
     };
 
     let monkey_4 = Monkey {
         name: Name::Four,
         held_items: vec![85, 91, 90],
-        calc_new_worry: Box::new(|old| old + 4),
+        increase_worry_level_closure: Rc::new(|old| old + 4),
         worry_divisor: 11,
         success_monkey: Name::Three,
         failure_monkey: Name::One,
+        inspections: 0,
     };
 
     let monkey_5 = Monkey {
         name: Name::Five,
         held_items: vec![67, 96, 69, 55, 70, 83, 62],
-        calc_new_worry: Box::new(|old| old * 2),
+        increase_worry_level_closure: Rc::new(|old| old * 2),
         worry_divisor: 3,
         success_monkey: Name::Four,
         failure_monkey: Name::One,
+        inspections: 0,
     };
 
     let monkey_6 = Monkey {
         name: Name::Six,
         held_items: vec![53, 86, 98, 70, 64],
-        calc_new_worry: Box::new(|old| old + 6),
+        increase_worry_level_closure: Rc::new(|old| old + 6),
         worry_divisor: 7,
         success_monkey: Name::Seven,
         failure_monkey: Name::Zero,
+        inspections: 0,
     };
 
     let monkey_7 = Monkey {
         name: Name::Seven,
         held_items: vec![88, 64],
-        calc_new_worry: Box::new(|old| old * old),
+        increase_worry_level_closure: Rc::new(|old| old * old),
         worry_divisor: 17,
         success_monkey: Name::Two,
         failure_monkey: Name::Five,
+        inspections: 0,
     };
 
     [
@@ -91,7 +138,7 @@ pub fn init_monkeys() -> [Monkey; 8] {
     ]
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 pub enum Name {
     Zero,
     One,
